8402f64bedd054a34f20cf60093193ee184ae9ba
arksohail <arksohail@gmail.com>
2024-09-29 19:52:25 +0530

    Managing HTTP-loaded Data via a Service

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -4,6 +4,7 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
 import { PlacesComponent } from '../places.component';
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-places',
@@ -13,21 +14,19 @@ import { PlacesService } from '../places.service';
   imports: [PlacesContainerComponent, PlacesComponent],
 })
 export class UserPlacesComponent implements OnInit {
-  places = signal<Place[] | undefined>(undefined);
   isFetching = signal<boolean>(false);
   error = signal('');
-
+  subscription?: Subscription;
+  
   private _placesService = inject(PlacesService);
   private destroyRef = inject(DestroyRef);
-
+  
+  places = this._placesService.loadedUserPlaces;
+  
   ngOnInit(): void {
     this.isFetching.set(true);
-    const subscription = this._placesService.loadUserPlaces()
+    this.subscription = this._placesService.loadUserPlaces()
       .subscribe({
-        next: (resp: Place[]) => {
-          console.log(resp);
-          this.places.set(resp);
-        },
         error: (err: Error) => {
           this.error.set(err.message);
         },
@@ -35,6 +34,6 @@ export class UserPlacesComponent implements OnInit {
           this.isFetching.set(false);
         }
       });
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+    this.destroyRef.onDestroy(() => this.subscription?.unsubscribe());
   }
 }
