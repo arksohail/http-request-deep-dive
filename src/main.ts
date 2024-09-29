170db6aeff185a68b316071864f87bec29de1114
arksohail <arksohail@gmail.com>
2024-09-29 21:01:27 +0530

    Introducing HTTP Response Interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function logginInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
   console.log("OUTGOING REQUEST INTERCEPTED");
@@ -9,7 +10,17 @@ function logginInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
   // const request = req.clone({
   //   headers: req.headers.set('X-DEBUG', 'TESTING'),
   // })
-  return next(req);
+  return next(req).pipe(
+    tap({
+      next: (event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log("INCOMIN RESPONSE INTERCEPTED");
+          console.log(event.status);
+          console.log(event.body);                   
+        }
+      },
+    })
+  );
 }
 
 bootstrapApplication(AppComponent,  {
