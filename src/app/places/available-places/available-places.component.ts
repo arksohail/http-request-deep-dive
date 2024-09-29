eb54fff85861b93f6ec22d4a7f5aa68a4925bd6d
arksohail <arksohail@gmail.com>
2024-09-29 16:21:36 +0530

    Sending a GET Request To Fetch Data

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-available-places',
@@ -12,10 +13,23 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
 
   private httpClient = inject(HttpClient);
+
+  private destroyRef = inject(DestroyRef);
   
   // constructor(private _httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    const subscription: Subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places').subscribe({
+      next: (resp) => {
+        console.log(resp.places);
+      },
+    });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
